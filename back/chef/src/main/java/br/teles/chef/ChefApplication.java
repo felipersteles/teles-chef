a91e23b3ae2d78864d937298662c72c73d4555df
Felipe Teles <felipersteles@gmail.com>
2023-07-11 09:15:33 -0300

    add: create backend

---
+++ back/chef/src/main/java/br/teles/chef/ChefApplication.java
@@ -0,0 +1,13 @@
+package br.teles.chef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChefApplication.class, args);
+	}
+
+}
---
+++ back/chef/src/test/java/br/teles/chef/ChefApplicationTests.java
@@ -0,0 +1,13 @@
+package br.teles.chef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
